bb5ca5d0b4ac6d340c52a76c07ac3371090fbbdb
massaut <nathan.roth67@yahoo.fr>
2019-10-02 13:38:18 +0200

    finish pqueue implementation

--- include/pqueue.h
+++ include/pqueue.h
@@ -2,30 +2,53 @@
 #define PQUEUE_H
 
 #include "svm_cell.h"
+#include <deque>
 #include <ostream>
-#include <queue>
+#include <sstream>
 #include <vector>
-
 template <typename T>
 class PQueue
 {
 public:
     PQueue();
 
-    void push(SVMCell<T> &cell, int level);
-    SVMCell<T> &pop(int level);
+    // push <cell> in queue at level <level>
+    void push(SVMCell<T> *cell, std::size_t level);
+    // pop the cell at <level> and returns it
+    SVMCell<T> *pop(std::size_t level);
 
+    // get the number of queues up to the last non-empty queue
+    std::size_t size() const;
+    // check if the PQueue is empty
     bool empty() const;
-    bool levelIsEmpty() const;
+    // check if a certain queue in a PQueue is empty
+    bool levelIsEmpty(std::size_t level) const;
 
     /*debug:
     Pqueue pq;
     std:cout << pqueue << std::endl;
     */
-    friend std::ostream &operator<<(std::ostream &o, const PQueue &pq);
+    friend std::ostream &operator<<(std::ostream &o, const PQueue &pq)
+    {
+        std::stringstream ss;
+        ss << "{";
+        for (auto q : pq.m_pqueue)
+        {
+            ss << "[";
+            for (int i = 0; i < q.size(); i++)
+            {
+                ss << q[i]->value() << " ";
+            }
+            ss << "] ";
+        }
+        ss << "}";
+        return o << ss.str();
+    }
 
 private:
-    std::vector<std::queue<SVMCell<T> &>> m_pqueue;
+    void clean();
+
+    std::vector<std::deque<SVMCell<T> *>> m_pqueue;
 };
 
 #endif
--- src/pqueue.cpp
+++ src/pqueue.cpp
@@ -1,5 +1,4 @@
 #include "pqueue.h"
-#include <sstream>
 
 template <typename T>
 PQueue<T>::PQueue()
@@ -7,53 +6,82 @@ PQueue<T>::PQueue()
 }
 
 template <typename T>
-void PQueue<T>::push(SVMCell<T> &cell, int level)
+void PQueue<T>::push(SVMCell<T> *cell, std::size_t level)
 {
-    if (level >= m_pqueue.size)
+    if (level >= m_pqueue.size())
     {
-        for (int l = m_pqueue.size(); l < level + 1; l++)
+        for (std::size_t l = m_pqueue.size(); l < level + 1; l++)
         {
-            m_pqueue.push_back(std::queue<SVMCell<T>>());
+            m_pqueue.push_back(std::deque<SVMCell<T> *>());
         }
     }
     // push cell
-    m_pqueue.at(level).push(cell);
+    m_pqueue.at(level).push_back(cell);
 }
 
 template <typename T>
-SVMCell<T> &PQueue<T>::pop(int level)
+SVMCell<T> *PQueue<T>::pop(std::size_t level)
 {
-    SVMCell<T> &cell = m_pqueue.at(level).pop();
+    SVMCell<T> *cell = m_pqueue.at(level).front();
+    m_pqueue.at(level).pop_front();
     if (m_pqueue.at(level).empty() && level == m_pqueue.size() - 1)
     {
-        // supprimer la queue
+        m_pqueue.pop_back();
     }
+    clean();
+    return cell;
+}
+
+template <typename T>
+std::size_t PQueue<T>::size() const
+{
+    return m_pqueue.size();
 }
 
 template <typename T>
 bool PQueue<T>::empty() const
 {
-}
-
-template <typename T>
-bool PQueue<T>::levelIsEmpty() const
-{
-}
-
-template <typename T>
-std::ostream &operator<<(std::ostream &o, const PQueue<T> &pq)
-{
-    std::stringstream ss;
-    ss << "{";
-    for (auto q : pq.m_pqueue)
+    std::size_t i = m_pqueue.size() - 1;
+    while (i >= 0)
     {
-        ss << "[";
-        for (int i = 0; i < q.size(); i++)
+        if (!m_pqueue.at(i).empty())
         {
-            ss << q[i] << " ";
+            return false;
         }
-        ss << "] ";
     }
-    ss << "}";
-    return o << ss.str();
+    return true;
 }
+
+template <typename T>
+bool PQueue<T>::levelIsEmpty(std::size_t level) const
+{
+    if (level < m_pqueue.size())
+    {
+        return m_pqueue.at(level).empty();
+    }
+    else
+    {
+        // level doesn't exist, therefore it is empty
+        return true;
+    }
+}
+
+template <typename T>
+void PQueue<T>::clean()
+{
+    int i = m_pqueue.size() - 1;
+    while (i >= 0 && m_pqueue.at(i).empty())
+    {
+        m_pqueue.pop_back();
+        i--;
+    }
+}
+
+template class PQueue<int>;
+template class PQueue<unsigned int>;
+template class PQueue<char>;
+template class PQueue<unsigned char>;
+template class PQueue<long>;
+template class PQueue<unsigned long>;
+template class PQueue<float>;
+template class PQueue<double>;
