50a6d29b92ab2cad85312d8dba12a2a3dc74426e
massaut <nathan.roth67@yahoo.fr>
2019-10-02 11:18:17 +0200

    tos class

---
+++ include/tos.h
@@ -0,0 +1,17 @@
+#ifndef TOS_H
+#define TOS_H
+
+#include "pqueue.h"
+#include "svm_img.h"
+
+template <typename T>
+class TOS
+{
+public:
+    TOS();
+    void genTree(SVMImage<T> img);
+
+private:
+};
+
+#endif // TOS_H
---
+++ src/tos.cpp
@@ -0,0 +1,12 @@
+#include "tos.h"
+
+template <typename T>
+TOS<T>::TOS()
+{
+}
+
+template <typename T>
+void TOS<T>::genTree(SVMImage<T> img)
+{
+    PQueue<T> q;
+}
