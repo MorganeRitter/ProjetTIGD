60cd5d7e2411e81ccc85d098f03edbe1f84bb6c6
massaut <nathan.roth67@yahoo.fr>
2019-10-09 11:16:26 +0200

    add SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "pqueue.h"
 #include "svm_cell.h"
 #include <Common/Image.h>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 using namespace LibTIM;
@@ -21,12 +22,44 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         std::cout << "Usage: " << argv[0] << " <image.pgm> \n";
-        exit(1);
+        //exit(1);
     }
     // Image est une classe générique paramétrée par le type des points contenus dans l'image
     Image<unsigned char> im;
     if (Image<U8>::load(argv[1], im))
         std::cout << "Great, image is loaded\n";
-    else
-        return 1;
+    //else
+    //return 1;
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Tree of Shape", sf::Style::Default, settings);
+
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::KeyPressed)
+            {
+                switch (event.key.code)
+                {
+                case sf::Keyboard::Escape:
+                    window.close();
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+
+        window.clear();
+
+        window.display();
+    }
+
+    return 0;
 }
