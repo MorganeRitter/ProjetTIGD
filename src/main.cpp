280535f4d097be4eef2a5212c716e44672034ce5
massaut <nathan.roth67@yahoo.fr>
2019-10-11 10:31:10 +0200

    image draw test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "pqueue.h"
 #include "svm_cell.h"
+#include "svm_img.h"
 #include <Common/Image.h>
 #include <SFML/Graphics.hpp>
 #include <iostream>
@@ -9,27 +10,27 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
-    PQueue<int> pq;
-
-    pq.push(new SVMCell<int>(CellType::Original, 1), 2);
-    pq.push(new SVMCell<int>(CellType::Original, 3), 0);
-    pq.push(new SVMCell<int>(CellType::Original, 4), 0);
-    pq.pop(0);
-
-    std::cout << pq << " | " << (pq.empty() == true ? "true" : "false") << std::endl;
-
     if (argc != 2)
     {
         std::cout << "Usage: " << argv[0] << " <image.pgm> \n";
-        //exit(1);
+        exit(1);
     }
     // Image est une classe générique paramétrée par le type des points contenus dans l'image
     Image<unsigned char> im;
     if (Image<U8>::load(argv[1], im))
         std::cout << "Great, image is loaded\n";
-    //else
-    //return 1;
+    else
+        return 1;
+
+    SVMImage<U8> svm_img(im);
+    std::cout << "SVM object created\n";
+    sf::Image img;
+    img = svm_img.asSFImage();
+    std::cout << " created sf::Image" << std::endl;
+    sf::Texture tex;
+    tex.loadFromImage(img);
+    sf::Sprite sprite;
+    sprite.setTexture(tex);
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
@@ -57,6 +58,7 @@ int main(int argc, char *argv[])
         }
 
         window.clear();
+        window.draw(sprite);
 
         window.display();
     }
